f8c4f2de3da8709a124db6c815605013dce041a6
Jacob Gurley <Jacob.Gurley@student.csulb.edu>
2023-03-25 16:55:37 -0700

    Updated look of Homepage, ContactUs

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -24,45 +24,17 @@
           </a>
         </div>
       </div>
-      <div class="contact-form-container">
-        <h2>Send us a message</h2>
-        <form class="contact-form" @submit.prevent="submitForm">
-          <input ref="nameInput" type="text" placeholder="Your Name" required />
-          <input ref="emailInput" type="email" placeholder="Your Email" required />
-          <textarea ref="messageInput" placeholder="Your Message" required></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
     </div>
   </div>
 </template>
 
 <script scoped>
 import Nav from "./NavBar.vue";
-import axios from "axios"; 
+ 
 
 export default {
   components: {
     Nav,
-  },
-  methods: {
-    async submitForm() {
-      const nameInput = this.$refs.nameInput.value;
-      const emailInput = this.$refs.emailInput.value;
-      const messageInput = this.$refs.messageInput.value;
-
-      try {
-        await axios.post("http://localhost:3000/send", {
-          name: nameInput,
-          email: emailInput,
-          message: messageInput,
-        });
-        alert("Email sent successfully!");
-      } catch (error) {
-        console.error("Error sending email:", error);
-        alert("Error sending email. Please try again later.");
-      }
-    },
   },
 };
 </script>
@@ -131,50 +103,6 @@ export default {
   color: black;
   font-weight: bold;
   font-size: 1.1em;
-}
-
-.contact-form-container {
-  width: 100%;
-  max-width: 600px;
-}
-
-.contact-form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.contact-form input,
-.contact-form textarea {
-  width: 100%;
-  padding: 10px;
-  font-family: var(--font-family);
-  font-size: 1em;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.contact-form textarea {
-  resize: none;
-  height: 150px;
-}
-
-.contact-form button {
-  font-family: var(--font-family);
-  font-weight: bold;
-  font-size: 1em;
-  padding: 10px;
-  background-color: #4a4a4a;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease-in-out;
-  margin-bottom: 30px;
-}
-
-.contact-form button:hover {
-  background-color: #0d2d5a;
 }
 
 /* Media query for mobile responsiveness */
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,120 +1,81 @@
 <template>
   <Nav />
-  <div class="width">
-    <div class="Winter">Welcome to the Winter 2023 Season!</div>
-    <div class="ChampWrapper">
-    <div class="Champions">Congratulations to the 2022 Summer Champions!</div>
-    <div class="Thur">Thursday Night Champs: LFG</div>
-    <div class="DivA">Sunday Night Div A Champs: Practice Squad</div>
-    <div class="DivB">Sunday Night Div B Champs: 9-5ers</div>
-    </div>
+  <div class="container">
+    <h1>Welcome to the Spring 2023 Season!</h1>
+    <section class="champions-section">
+      <h2>Congratulations to the 2022 Summer Champions!</h2>
+      <ul>
+        <li>Thursday Night Champs: LFG</li>
+        <li>Sunday Night Div A Champs: Practice Squad</li>
+        <li>Sunday Night Div B Champs: 9-5ers</li>
+      </ul>
+    </section>
     <div class="carousel-wrapper">
       <carousel :slides="slides" :interval="3000" controls indicators></carousel>
-    
-
+    </div>
   </div>
-</div>
-  
 </template>
 
 <script scoped>
 import Nav from "./NavBar.vue";
-import Carousel from "./CarouselFinal.vue"
+import Carousel from "./CarouselFinal.vue";
 export default {
   components: {
-  Nav,
-  Carousel,
-  
+    Nav,
+    Carousel,
+  },
 
-},
-
-data: () => ({
-  slides: [
-    require('../assets/thursday.jpg'),
-    require('../assets/sundaydivA.jpg'),
-    require('../assets/sundaydivB.jpg'),
-  ],
-}),
-
+  data: () => ({
+    slides: [
+      require("../assets/thursday.jpg"),
+      require("../assets/sundaydivA.jpg"),
+      require("../assets/sundaydivB.jpg"),
+    ],
+  }),
 };
-
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.width{
-  width:100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 15px;
   text-align: center;
 }
 
-.Winter{
-  text-align: center;
-  color:black;
-  font:bold;
-  display: block;
-  font-size: xx-large;
-  font-family: Helvetica;
+h1 {
+  font-family: "Helvetica", sans-serif;
+  font-weight: bold;
+  font-size: 2.5rem;
   margin-bottom: 30px;
 }
-.Champions{
-  text-align: center;
-  color:black;
-  font:bold;
-  display: block;
-  font-size: large;
-  font-family: Helvetica;
-  margin-bottom: 10px;
 
+.champions-section {
+  border: 5px solid #0d2d5a;
+  padding: 1rem;
+  margin-bottom: 30px;
 }
 
-.Thur{
-  text-align: center;
-  color:black;
-  font:bold;
-  display: block;
-  font-size: large;
-  font-family: Helvetica;
-
+h2 {
+  font-family: "Helvetica", sans-serif;
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-bottom: 15px;
 }
 
-.DivA{
-  text-align: center;
-  color:black;
-  font:bold;
-  display: block;
-  font-size: large;
-  font-family: Helvetica;
-
+ul {
+  list-style: none;
+  padding: 0;
 }
 
-.DivB{
-  text-align: center;
-  color:black;
-  font:bold;
-  display: block;
-  font-size: large;
-  font-family: Helvetica;
-
+li {
+  font-family: "Helvetica", sans-serif;
+  font-weight: bold;
+  font-size: 1.2rem;
 }
-.carousel-wrapper{
+
+.carousel-wrapper {
   border: 5px solid #0d2d5a;
   padding: 5px;
-  min-width: 992px;
-  max-width: 960px;
-  margin-left: auto;
-  margin-right: auto; 
-}
-
-.ChampWrapper{
-  border: 5px solid #0d2d5a;
-  padding: 5px;
-  min-width: 992px;
-  max-width: 960px;
-  margin-left: auto;
-  margin-right: auto;
 }
 </style>
