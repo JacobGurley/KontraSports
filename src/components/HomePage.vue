8590684985966167310e04123bc9f54935f99635
JacobGurley <united.states.productions@gmail.com>
2025-12-03 20:31:36 -0800

    Added Winter 2026 Season reg msg and link

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <h1>FALL 2025 SEASON STARTS SUNDAY OCTOBER 5TH & THURSDAY OCTOBER 16TH!</h1>
+    <h1>REGISTRATION FOR WINTER 2026 SEASON IS NOW OPEN! SEASON STARTS JANUARY 11TH</h1>
     <h2>Click the Registration Link Below to Reserve Your Spot</h2>
     <div class="links">
       <p class="reg">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdUlHfOk92NlLWwgmSKHkZ5UGCgnVzsxTYfIXMLoRk6QI1cig/viewform" target="_blank" rel="noopener noreferrer">Click Here for 2025 Fall Registration </a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdQOmCHDV8hvhppvq5-bKbflQ6VIOxsQzzfHIMWe_kt7Z8yVQ/viewform" target="_blank" rel="noopener noreferrer">Click Here for 2026 Winter Registration </a>
       </p>
       <p class="waiver">
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdJBicpbOetgqkq3pn8_E8mKaudZTIaLveak6eWJc_Nqru7dg/viewform?pli=1" target="_blank" rel="noopener noreferrer">Click Here for the Waiver Form </a>
