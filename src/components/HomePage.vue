c43a2638d30138ffcac43b27d84db38ddd8877df
JacobGurley <united.states.productions@gmail.com>
2026-06-15 20:26:48 -0700

    Summer Reg link added

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <h1>WELCOME TO THE SPRING 2026 SEASON!</h1>
+    <h1>REGISTRATION FOR SUMMER 2026 SEASON NOW OPEN!</h1>
     <h2>Click the Registration Link Below to Reserve Your Spot</h2>
     <div class="links">
       <p class="reg">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSelUVEWq8ZP7RRgUWl2YynN2ErFvjYcEQ0eY_OMMhKWE-gmGA/viewform" target="_blank" rel="noopener noreferrer">Click Here for 2026 Spring Registration </a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdxzykMPU28wDJ0VfpOjSc0AgmbR_m0X0_bj3C7g2aWPdUP3Q/viewform" target="_blank" rel="noopener noreferrer">Click Here for 2026 Summer Registration </a>
       </p>
       <p class="waiver">
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdJBicpbOetgqkq3pn8_E8mKaudZTIaLveak6eWJc_Nqru7dg/viewform?pli=1" target="_blank" rel="noopener noreferrer">Click Here for the Waiver Form </a>
